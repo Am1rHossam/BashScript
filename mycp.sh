6511ef800bbabc6b2eac0b9f5fbf4942358298f8
Amir Hossam <amir_h56395@cic-cairo.com>
2022-02-03 12:07:53 +0200

    Task 3

---
+++ mycp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#Copying multile files to newcopy folder
+cp *.sh /home/ubuntu/Desktop/bash_file/newcopy
+#copy s1.sh to newcopy folder 
+cp s1.sh /home/ubuntu/Desktop/bash_file/newcopy/s3.sh
+
+
