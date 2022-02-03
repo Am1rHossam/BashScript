fc80122a69a4532d7387b0129a7397505911e1dc
Amir Hossam <amir_h56395@cic-cairo.com>
2022-02-03 12:11:14 +0200

    Task 1

---
+++ bash1.sh
@@ -0,0 +1,10 @@
+#1. Create a script that asks for user name then send a greeting to him.
+#!usr/bin/bash
+
+echo "please enter your name:"
+read name
+
+echo "please enter your age:"
+read age
+
+echo "Hello, $name , you are $age"
